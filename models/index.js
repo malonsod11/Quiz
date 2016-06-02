3b72c5bbb9c8a1bbe011635130d90140b4a907ee
Martín Alonso <martin.alonso.diaz@alumnos.upm.es>
2016-06-03 00:23:50 +0200

    Tema9: Despliegue DB en Heroku

--- models/index.js
+++ models/index.js
@@ -3,8 +3,23 @@ var path=require('path');
 //Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite:
-var sequelize=new Sequelize(null,null,null,{dialect: "sqlite", storage: "quiz.sqlite" });
+// Usar BBDD SQLite:
+// DATABASE_URL = sqlite:///
+// DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+// DATABASE_URL = postgres://user:passwd@host:port/database
+var url, storage;
+if (!process.env.DATABASE_URL) {
+url = "sqlite:///";
+storage = "quiz.sqlite";
+} else {
+url = process.env.DATABASE_URL;
+storage = process.env.DATABASE_STORAGE || "";
+}
+var sequelize = new Sequelize(url,
+{ storage: storage,
+omitNull: true
+});
 
 //Importar la definición de la tabla Quiz de quiz.js
 var Quiz=sequelize.import(path.join(__dirname,'quiz'));
